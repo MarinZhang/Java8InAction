bf4e1299b4e7d02609f79d91a644cd227b5a31c3
MarinZhang <1475705241@qq.com>
2019-08-23 00:45:49 +0800

    尝试练习Java8_lambda的写法=>练习lamda应用于读文件

--- src/main/java/lambdasinaction/chap3/ExecuteAround.java
+++ src/main/java/lambdasinaction/chap3/ExecuteAround.java
@@ -6,35 +6,44 @@ public class ExecuteAround {
 	public static void main(String ...args) throws IOException{
 
         // method we want to refactor to make more flexible
-        String result = processFileLimited();
-        System.out.println(result);
+//        String result = processFileLimited();
+//        System.out.println(result);
 
         System.out.println("---");
 
 		String oneLine = processFile((BufferedReader b) -> b.readLine());
 		System.out.println(oneLine);
 
-		String twoLines = processFile((BufferedReader b) -> b.readLine() + b.readLine());
+		// 打印两行
+		String twoLines = processFile(
+				(BufferedReader b) -> b.readLine() + b.readLine()
+		);
 		System.out.println(twoLines);
 
 	}
 
-    public static String processFileLimited() throws IOException {
-        try (BufferedReader br =
-                     new BufferedReader(new FileReader("lambdasinaction/chap3/data.txt"))) {
-            return br.readLine();
-        }
-    }
+//    public static String processFileLimited() throws IOException {
+//        try (BufferedReader br =
+//                     new BufferedReader(new FileReader("lambdasinaction/chap3/data.txt"))) {
+//            return br.readLine();
+//        }
+//    }
 
 
 	public static String processFile(BufferedReaderProcessor p) throws IOException {
-		try(BufferedReader br = new BufferedReader(new FileReader("lambdasinaction/chap3/data.txt"))){
+		try(
+				BufferedReader br = new BufferedReader(new FileReader("lambdasinaction/chap3/data.txt"))
+		){
 			return p.process(br);
 		}
 
 	}
-
+	
+	/**
+	 * 接口(函数式写法)
+	 */
 	public interface BufferedReaderProcessor{
+		// 1-匹配 BufferedReader -> String ; 2-能抛出IO异常
 		public String process(BufferedReader b) throws IOException;
 
 	}
